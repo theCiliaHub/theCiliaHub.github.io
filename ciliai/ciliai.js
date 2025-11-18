fc02392fd29e14473fd3d7dd4725c620c4c93fb5
CiliaHub <humandiseaselab@gmail.com>
2025-11-18 12:08:19 +0300

    Update ciliai.js

--- ciliai/ciliai.js
+++ ciliai/ciliai.js
@@ -208,7 +208,7 @@ async function initCiliAI() {
      * Fetches the pre-compiled database files from GitHub.
      * (MODIFIED Nov 18 2025): Added cellDataCache builder for advanced UMAP plotting
      */
-    async function loadCiliAIData(timeoutMs = 60000) {
+ window.loadCiliAIData = async function(timeoutMs = 60000) {
         const baseUrl = 'https://raw.githubusercontent.com/theCiliaHub/theCiliaHub.github.io/refs/heads/main/';
         const mainDbUrl = baseUrl + 'ciliAI_master_database.json';
         const lookupsUrl = baseUrl + 'ciliAI_lookups.json';
